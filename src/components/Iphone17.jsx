3efe4e2d44e8ba46a1d48943edd0a4b43c51d1b7
yahya-zakariaa <yz3004629@gmail.com>
2025-09-15 19:25:10 +0300

    first commit

--- src/components/Iphone17.jsx
+++ src/components/Iphone17.jsx
@@ -80,7 +80,7 @@ export default function Iphone17() {
                     fill
                     quality={100}
                     unoptimized
-                    className=" mx-auto  md:max-w-[100px] max-w-[90px] md:max-h-[120px] max-h-[120px]"
+                    className=" mx-auto  md:max-w-[100px] max-w-[90px] md:max-h-[120px] max-h-[115px]"
                   />
                 </div>
                 <div className="flex flex-col justify-between sm:px-3 w-full sm:max-w-[70%]">
